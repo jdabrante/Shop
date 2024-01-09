f158e2e3ee9a699bd48e650190b6a728de72bd77
Juan Dimas Abrante Hernández <19jdah98@gmail.com>
2024-01-09 02:07:51 +0000

    Finished the chapter 9

--- payment/webhooks.py
+++ payment/webhooks.py
@@ -4,6 +4,7 @@ from django.conf import settings
 from django.http import HttpResponse
 from django.views.decorators.csrf import csrf_exempt
 from orders.models import Order
+from .tasks import payment_completed
 
 @csrf_exempt
 def stripe_webhook(request):
@@ -29,4 +30,5 @@ def stripe_webhook(request):
             order.paid = True
             order.stripe_id = session.payment_intent
             order.save()
+            payment_completed.delay(order.id)
     return HttpResponse(status=200)
